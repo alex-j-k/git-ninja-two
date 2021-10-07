5a2822577defcb179d88871effe31e9286e66cba
alex-j-k <63852006+alex-j-k@users.noreply.github.com>
2021-10-07 17:48:54 +1000

    added index html

---
+++ index.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>ninja two</title>
+    <h1>info</h1>
+    <h2>more info</h2>
+    <h3>qwertyuiop</h3>
+    <p>mnjujjthybdhjsoppwolsokd  jsnhjkyehd bhshsyteyb amnnkjsskiwuqiqosnd </p>
+    <p>mnjujjthybdhjsoppwolsokd  jsnhjkyehd bhshsyteyb amnnkjsskiwuqiqosnd </p>
+    <p>mnjujjthybdhjsoppwolsokd  jsnhjkyehd bhshsyteyb amnnkjsskiwuqiqosnd </p>
+    <p>mnjujjthybdhjsoppwolsokd  jsnhjkyehd bhshsyteyb amnnkjsskiwuqiqosnd </p>
+
+
+</head>
+<body>
+    
+</body>
+</html>
